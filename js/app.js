ae3798c85753d7dfc7409f51417a4e1770c05a41
emmikoivisto <emetsi@gmail.com>
2020-10-15 16:39:17 +0100

    reading list

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener("submit", handleSubmit);
+});
+
+  const handleSubmit = function(event) {
+    event.preventDefault()
+    const toReadList = document.querySelector("#reading-list")
+    const listItem = document.createElement('li');
+    listItem.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`
+    toReadList.appendChild(listItem)
+    document.getElementById("new-item-form").reset();
+  }; 
+  
+
+
+  function deleteAll() {
+    const listItem = document.getElementById('li');
+    toReadList.removeChild(listItem);
+   }
